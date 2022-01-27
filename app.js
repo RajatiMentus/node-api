d4d37ad263f20e7b36455bbe5e53b0eb4303d2b1
RajatiMentus <98326858+RajatiMentus@users.noreply.github.com>
2022-01-27 18:29:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,4 +26,6 @@ client.connect(function(err) {
         });
     });
 });
-app.listen(3000, () => console.log('Listening on port 3000..'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
